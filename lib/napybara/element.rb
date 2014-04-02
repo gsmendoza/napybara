3d66f1146cd25170a916a0631d2e00e55b0a6c87
George Mendoza <gsmendoza@gmail.com>
2014-04-02 10:29:26 +0800

    Refactor Element::Selector.

--- lib/napybara/element.rb
+++ lib/napybara/element.rb
@@ -12,33 +12,34 @@ module Napybara
 
     def finder(child_element_name, child_element_selector, method_name = :id, &block)
       self.define_singleton_method(child_element_name) do |record = nil|
-        selector =
-          if record
-            record_id = record && record.public_send(method_name)
-            "#{child_element_selector}#{record_id}"
-          else
-            child_element_selector
-          end
-
-        self.class.new(self.get.find(selector), &block)
+        selector = Selector.new(child_element_selector, method_name, record)
+        self.class.new(self.get.find(selector.to_s), &block)
       end
 
       self.define_singleton_method("has_#{child_element_name}?") do |record|
-        record_id = record && record.public_send(method_name)
-        selector = "#{child_element_selector}#{record_id}"
-        self.get.has_css?(selector)
+        selector = Selector.new(child_element_selector, method_name, record)
+        self.get.has_css?(selector.to_s)
       end
 
       self.define_singleton_method("has_no_#{child_element_name}?") do |record|
-        record_id = record && record.public_send(method_name)
-        selector = "#{child_element_selector}#{record_id}"
-        self.get.has_no_css?(selector)
+        selector = Selector.new(child_element_selector, method_name, record)
+        self.get.has_no_css?(selector.to_s)
       end
 
       self.define_singleton_method(child_element_name.to_s.pluralize) do
         self.get.all(child_element_selector).map do |child_element|
           self.class.new(child_element, &block)
         end
+      end
+    end
+
+    class Selector < Struct.new(:child_element_selector, :method_name, :record)
+      def record_id
+        record && record.public_send(method_name)
+      end
+
+      def to_s
+        "#{child_element_selector}#{record_id}"
       end
     end
   end
